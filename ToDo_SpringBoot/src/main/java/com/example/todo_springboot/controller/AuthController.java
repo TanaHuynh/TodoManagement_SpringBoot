6f9f8466d6b471611a210b6b12904117395f2df5
Huynh Thi Hong Thanh <thanh.huynh.9111994@gmail.com>
2025-03-01 13:02:02 +0700

    Registration and Login Module - Spring Boot Backend

---
+++ ToDo_SpringBoot/src/main/java/com/example/todo_springboot/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.example.todo_springboot.controller;
+
+import com.example.todo_springboot.dto.LoginDto;
+import com.example.todo_springboot.dto.RegisterDto;
+import com.example.todo_springboot.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private AuthService authService;
+
+    // Build Register REST API
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+        String response = authService.register(registerDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    // Build Login REST API
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        String response = authService.login(loginDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
